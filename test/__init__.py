83e6cdf54b4b6a4bc792cfc828d4eb173e803750
Audrey Germain <audrey.germain2@canada.ca>
2021-06-17 19:18:05 +0000

    fix tests TMP_PATH

--- test/__init__.py
+++ test/__init__.py
@@ -3,5 +3,6 @@ import os
 
 HOST_NUM = os.getenv("TRUE_HOST")[-1]
 USER = os.getenv("USER")
+SPOOKI_TMPDIR = os.getenv("SPOOKI_TMPDIR")
 TEST_PATH = "/fs/site%s/eccc/cmd/w/spst900/spooki/spooki_dir/pluginsRelatedStuff/"%HOST_NUM
-TMP_PATH = "/fs/site%s/eccc/cmd/w/%s/spooki_tmpdir/"%(HOST_NUM,USER)
+TMP_PATH = SPOOKI_TMPDIR if SPOOKI_TMPDIR else "/home/spst900/spooki/spooki_tmpdir_ppp%s/%s"%(HOST_NUM,USER)

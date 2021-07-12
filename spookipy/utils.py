4d1b345ae0e0bf89d6dedc3d2af1a982dafae63a
Sébastien Fortier <sebastien.fortier@canada.ca>
2021-07-12 13:32:57 +0000

    add select_with_meta and metadata cleanup functions

--- spookipy/utils.py
+++ spookipy/utils.py
@@ -2,6 +2,8 @@
 import inspect
 from functools import wraps
 import pandas as pd
+import rpnpy.librmn.all as rmn
+
 
 def initializer(func):
     """
@@ -142,3 +144,231 @@ def validate_nomvar(nomvar:str, caller_class:str, error_class:Exception):
     """
     if len(nomvar) > 4:
         raise error_class(caller_class + ' - max 4 char for nomvar')
+
+class SelectError(Exception):
+    pass
+
+def select_meta(df:pd.DataFrame) -> pd.DataFrame:
+    meta_df = df.query('nomvar in ["!!","P0","PT",">>","^^","^>","HY","!!SF"]')
+    return meta_df
+
+def select_with_meta(df:pd.DataFrame,nomvar:list) -> pd.DataFrame:
+    if df.empty:
+        raise SelectError(f'dataframe is empty - nothing to select into')
+
+    results = []
+
+    if len(nomvar) == 0:
+        raise SelectError(f'nomvar is empty - nothing to select')
+
+    for var in nomvar:
+        res_df = df.query(f'nomvar=="{var}"').reset_index(drop=True)
+        if res_df.empty:
+            raise SelectError(f'missing {var} in dataframe')
+        results.append(res_df)
+
+    meta_df = select_meta(df)
+
+    if not meta_df.empty:
+        results.append(meta_df)
+
+    selection_result_df = pd.concat(results,ignore_index=True)   
+
+    selection_result_df = metadata_cleanup(selection_result_df)
+
+    return selection_result_df
+
+    #    kind can take the following values
+    #    0, p est en hauteur (m) rel. au niveau de la mer (-20, 000 -> 100, 000)
+    #    1, p est en sigma                                (0.0 -> 1.0)
+    #    2, p est en pression (mb)                        (0 -> 1100)
+    #    3, p est un code arbitraire                      (-4.8e8 -> 1.0e10)
+    #    4, p est en hauteur (M) rel. au niveau du sol    (-20, 000 -> 100, 000)
+    #    5, p est en coordonnee hybride                   (0.0 -> 1.0)
+    #    6, p est en coordonnee theta                     (1 -> 200, 000)
+    #    10, p represente le temps en heure               (0.0 -> 1.0e10)
+    #    15, reserve (entiers)
+    #    17, p represente l'indice x de la matrice de conversion (1.0 -> 1.0e10)
+    #        (partage avec kind=1 a cause du range exclusif
+    #    21, p est en metres-pression                     (0 -> 1, 000, 000)
+    #                                                     fact=1e4
+    #        (partage avec kind=5 a cause du range exclusif)
+
+
+def get_kinds_and_ip1(df:pd.DataFrame) -> dict:
+    ip1s = df.ip1.unique()
+    kinds = {}
+    for ip1 in ip1s:
+        (_, kind) = rmn.convertIp(rmn.CONVIP_DECODE, int(ip1))
+        if kind not in kinds.keys():
+            kinds[kind] = ip1    
+    print(kinds)    
+    return kinds
+
+def get_ips(df:pd.DataFrame,sigma=False,hybrid=False) -> list:
+    kinds = get_kinds_and_ip1(df)
+    ip1_list = []
+    if sigma:
+        if 1 in kinds.keys():
+            ip1_list.append(kinds[1])
+    if hybrid:        
+        if 5 in kinds.keys():
+            ip1_list.append(kinds[5])    
+    return ip1_list  
+
+def get_model_ips(df:pd.DataFrame) -> list:
+    return get_ips(df,sigma=True,hybrid=True)
+    # kinds = get_kinds_and_ip1(df)
+
+    # ip1_list = []
+    # if 1 in kinds.keys():
+    #     ip1_list.append(kinds[1])
+    # if 5 in kinds.keys():
+    #     ip1_list.append(kinds[5])    
+    # return ip1_list    
+
+def get_sigma_ips(df:pd.DataFrame) -> list:
+    return get_ips(df,sigma=True)
+    # kinds = get_kinds_and_ip1(df)
+    # ip1_list = []
+    # if 1 in kinds.keys():
+    #     ip1_list.append(kinds[1])
+    # return ip1_list    
+
+def get_hybrid_ips(df:pd.DataFrame) -> list:
+    return get_ips(df,hybrid=True)
+    # kinds = get_kinds_and_ip1(df)
+    # ip1_list = []
+    # if 5 in kinds.keys():
+    #     ip1_list.append(kinds[5])
+    # return ip1_list    
+
+# def get_model_ips(df:pd.DataFrame) -> list:
+#     ip1s = df.ip1.unique()
+#     ip1_list = []
+#     for ip1 in ip1s:
+#         (_, kind) = rmn.convertIp(rmn.CONVIP_DECODE, int(ip1))
+#         if (kind == 1) or (kind == 5):
+#             ip1_list.append(ip1)
+#     return ip1_list    
+
+# def get_sigma_ips(df:pd.DataFrame) -> list:
+#     ip1s = df.ip1.unique()
+#     ip1_list = []
+#     for ip1 in ip1s:
+#         (_, kind) = rmn.convertIp(rmn.CONVIP_DECODE, int(ip1))
+#         if (kind == 1):
+#             ip1_list.append(ip1)
+#     return ip1_list  
+
+# def get_hybrid_ips(df:pd.DataFrame) -> list:
+#     ip1s = df.ip1.unique()
+#     ip1_list = []
+#     for ip1 in ip1s:
+#         (_, kind) = rmn.convertIp(rmn.CONVIP_DECODE, int(ip1))
+#         if (kind == 5):
+#             ip1_list.append(ip1)
+#     return ip1_list  
+
+def metadata_cleanup(df:pd.DataFrame):
+
+    if df.empty:
+        return df
+    
+    not_meta_df = df.query('nomvar not in  ["!!","P0","PT",">>","^^","^>","HY","!!SF"]').reset_index(drop=True)
+    
+    # get deformation fields
+    grid_deformation_fields_df = pd.DataFrame(dtype=object)
+    all_grids = not_meta_df.grid.unique()
+
+    df_list = []
+    for grid in all_grids:
+        df_list.append(df.query(f'(nomvar==">>") and (grid=="{grid}")').reset_index(drop=True))
+        df_list.append(df.query(f'(nomvar=="^^") and (grid=="{grid}")').reset_index(drop=True))
+        df_list.append(df.query(f'(nomvar=="^>") and (grid=="{grid}")').reset_index(drop=True))
+
+    if len(df_list):
+        grid_deformation_fields_df = pd.concat(df_list,ignore_index=True)
+
+    # get P0's
+    p0_fields_df = pd.DataFrame(dtype=object)
+    model_ips = get_model_ips(not_meta_df)
+    model_grids = set()
+    for ip1 in model_ips:
+        model_grids.add(not_meta_df.query(f'ip1=={ip1}').reset_index(drop=True).iloc[0]['grid'])
+    
+    df_list = []
+    for grid in list(model_grids):
+        # print(grid)
+        df_list.append(df.query(f'(nomvar=="P0") and (grid=="{grid}")').reset_index(drop=True))
+
+    if len(df_list):
+        p0_fields_df = pd.concat(df_list,ignore_index=True)
+    
+    #get PT's
+    pt_fields_df = pd.DataFrame(dtype=object)
+    sigma_ips = get_sigma_ips(not_meta_df)
+    sigma_grids = set()
+    for ip1 in sigma_ips:
+        sigma_grids.add(not_meta_df.query(f'ip1=={ip1}').reset_index(drop=True).iloc[0]['grid'])
+    
+    df_list = []
+    for grid in list(sigma_grids):
+        df_list.append(df.query(f'(nomvar=="PT") and (grid=="{grid}")').reset_index(drop=True))
+
+    if len(df_list):
+        pt_fields_df = pd.concat(df_list,ignore_index=True)
+
+    #get HY
+    hy_field_df = pd.DataFrame(dtype=object)
+    hybrid_ips = get_hybrid_ips(not_meta_df)
+    if len(hybrid_ips):
+        hy_field_df = df.query(f'nomvar=="HY"').reset_index(drop=True)
+
+    #get !!'s strict
+    toctoc_fields_df = pd.DataFrame(dtype=object)
+    hybrid_fields_df = pd.DataFrame(dtype=object)
+
+    if len(hybrid_ips):
+        hybrid_fields_df = df.query(f'ip1 in {hybrid_ips}').reset_index(drop=True)
+
+    grids = []
+    if not hybrid_fields_df.empty:
+        grids = hybrid_fields_df.grid.unique()
+
+    df_list = []
+    for grid in grids:
+        toctoc = df.query(f'(nomvar=="!!") and (grid=="{grid}")')
+        if not toctoc.empty:
+            toctoc = True
+        else:
+            toctoc = False
+        # toctoc, _, _, _, _, _, _ = fstpy.get_meta_fields_exists(grid)
+        if toctoc:
+            df_list.append(df.query(f'(nomvar=="!!") and (grid=="grid")'))
+
+    if len(df_list):
+        toctoc_fields_df = pd.concat(df_list,ignore_index=True)
+
+    df = pd.concat([not_meta_df,grid_deformation_fields_df,p0_fields_df,pt_fields_df,hy_field_df,toctoc_fields_df],ignore_index=True)
+
+    return df
+    # if len(hybrid_ips):
+    #     df.query(f'ip=="HY"').reset_index(drop=True)
+
+    #     #par defaut
+    #     else if ( mpds->second->getPdsName() == "!!")
+        
+    #         # only keep !! if we have at least one pds with a vcode equal to the ig1 value of the !!
+    #         int toctocVcode
+    #         LEXICALCAST(mpds->second->getOtherInformation().ig1,toctocVcode)
+    #         keep = hasPdsWithVcode(toctocVcode)
+        
+    #     # active par variable globale
+    #     else if ( mpds->second->getPdsName() == "!!")
+        
+    #         # only keep !! if we have at least one pds with a vcode equal to the ig1 value of the !!
+    #         int toctocVcode
+    #         LEXICALCAST(mpds->second->getOtherInformation().ig1,toctocVcode)
+    #         keep = hasPdsWithVcode(toctocVcode)
+        

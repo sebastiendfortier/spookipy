480f1c925316b7ae65f1b32598cc6b1f6eed496f
Sébastien Fortier <sebastien.fortier@canada.ca>
2021-07-14 15:21:14 +0000

    gen doc files

--- org/convert_and_send_to_doc.sh
+++ org/convert_and_send_to_doc.sh
@@ -4,15 +4,43 @@ echo ${DIR}
 cd ${DIR}
 DOC_DIR=${DIR:0:${#DIR}-3}doc
 echo ${DOCDIR}
-for f in `ls *.org`
-do
-   echo 'converting '$f&&pandoc -s $f -o ${DOC_DIR}/${f%.org}.rst
-done
+# for f in `ls *.org`
+# do
+#    echo 'converting '$f&&pandoc -s $f -o ${DOC_DIR}/${f%.org}.rst
+# done
 
 cd ../spookipy
 
 for f in `find . -name '*.org'`
 do 
    name=${f/.*\/}
-    echo 'converting '$f&&pandoc -s $f -o ../doc/${name%.org}.rst
+   name=${name%.org}
+   lower_name=`echo $name|tr [:upper:] [:lower:]`
+   file_to_write=../doc/$lower_name.rst
+   rm $file_to_write
+   eval printf '=%.0s' {1..${#name}} >> $file_to_write
+   printf "\n" >> $file_to_write
+   echo $name >> $file_to_write
+   eval printf '=%.0s' {1..${#name}} >> $file_to_write
+   printf "\n" >> $file_to_write
+   echo .. include:: $name.rst >> $file_to_write
+   printf "\n" >> $file_to_write
+   echo "Python Module"  >> $file_to_write
+   echo "-------------"  >> $file_to_write
+   printf "\n" >> $file_to_write
+   echo ".. automodule:: spookipy."$lower_name"."$lower_name >> $file_to_write
+   echo "   :members:" >> $file_to_write
+   printf "\n" >> $file_to_write
+   echo 'converting '$f&&pandoc -s $f -o ../doc/${name}.rst
 done
+# ==================
+# AddElementsByPoint
+# ==================
+
+# .. include:: AddElementsByPoint.rst
+
+# Python Module
+# -------------
+
+# .. automodule:: spookipy.addelementsbypoint.addelementsbypoint
+#    :members: 

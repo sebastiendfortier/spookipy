75fb25eee33fe5c86412bee39129720f6da20574
Sébastien Fortier <sebastien.fortier@canada.ca>
2021-08-11 11:52:29 +0000

    add missing plugins to interface

--- spookipy/all.py
+++ spookipy/all.py
@@ -1,22 +1,25 @@
-from .humidityutils import *
-
-from .plugin import *
-
+# -*- coding: utf-8 -*-
 from .addelementsbypoint import *
 from .addtoelements import *
+from .applyunary import *
 from .arithmeticmeanbypoint import *
+from .coriolisparameter import *
 from .dewpointdepression import *
+from .filterdigital import *
 from .georgekindex import *
 from .gridcut import *
 from .humidex import *
 from .humidityrelative import *
 from .humidityspecific import *
+from .humidityutils import *
 from .interpolationhorizontal import *
 from .mask import *
 from .minmaxlevelindex import *
 from .multiplyelementsby import *
 from .multiplyelementsbypoint import *
 from .opelementsbypoint import *
+from .opelementsbyvalue import *
+from .plugin import *
 from .pressure import *
 from .saturationvapourpressure import *
 from .setconstantvalue import *
@@ -26,7 +29,3 @@ from .watervapourmixingratio import *
 from .windchill import *
 from .windmax import *
 from .windmodulus import *
-
-
-from .applyunary import *
-from .opelementsbyvalue import *

e92129afa941d39e30efb518f8dfe609621ed2e2
Sébastien Fortier <sebastien.fortier@canada.ca>
2021-08-11 11:58:31 +0000

    add coriolis parameter plugin

---
+++ spookipy/coriolisparameter/coriolisparameter.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+from numpy import float32
+from ..utils import create_empty_result, get_existing_result, existing_results, final_results
+from ..plugin import Plugin
+import pandas as pd
+import fstpy.all as fstpy
+import sys
+import math
+import numpy as np
+
+
+def coriolis_parameter(lat_2d:np.ndarray) -> np.ndarray:
+    """computes the coriolis parameter for each latitude obtained from gdll (2d grid size latitudes)
+
+    :param lat_2d: latitudes obtained from gdll (2d grid size latitudes)
+    :type lat_2d: np.ndarray
+    :return: coriolis parameter
+    :rtype: np.ndarray
+    """
+    # 2 * omega = 0.00014584
+    return 0.00014584 * np.sin(lat_2d * (math.pi/180)).astype(float32)
+
+class CoriolisParameterError(Exception):
+    pass
+
+class CoriolisParameter(Plugin):
+
+    def __init__(self,df:pd.DataFrame):
+
+        self.plugin_result_specifications = {
+            'CORP':{'nomvar':'CORP','etiket':'CoriolisParameter','unit':'divergence','ip1':0,'ip2':0,'ip3':0,'datyp':134,'nbits':12}
+        }
+
+        self.df = df
+
+        self.validate_input()
+
+    def validate_input(self):
+        if self.df.empty:
+            raise CoriolisParameterError('No data to process')
+
+        self.df = fstpy.add_composite_columns(self.df,True,'numpy', attributes_to_decode=['unit'])
+
+        self.df = fstpy.metadata_cleanup(self.df)
+
+        # print(self.df[['nomvar','typvar','etiket','unit','surface','grid','forecast_hour']].sort_values(by=['grid','nomvar']).to_string())
+        self.meta_df = self.df.loc[self.df.nomvar.isin(["^^",">>","^>", "!!", "!!SF", "HY","P0","PT"])].reset_index(drop=True)
+
+        #check if result already exists
+        self.existing_result_df = get_existing_result(self.df,self.plugin_result_specifications)
+
+        self.groups = self.df.groupby(['grid'])
+
+    def compute(self) -> pd.DataFrame:
+        if not self.existing_result_df.empty:
+            return existing_results('CoriolisParameter',self.existing_result_df,self.meta_df)
+
+        sys.stdout.write('CoriolisParameter - compute\n')
+        df_list=[]
+        for _, current_group in self.groups:
+            latlon_df = fstpy.get_2d_lat_lon(current_group)
+            lat_df = latlon_df.loc[latlon_df.nomvar=='LA'].reset_index(drop=True)
+            if lat_df.empty:
+                continue
+            current_group = current_group.loc[~current_group.nomvar.isin(["^^",">>","^>", "!!", "!!SF", "HY","P0","PT"])].reset_index(drop=True)
+            if current_group.empty:
+                continue
+
+            corp_df = create_empty_result(lat_df,self.plugin_result_specifications['CORP'])
+            corp_df = ajust_column_values(current_group, corp_df)
+
+            for i in corp_df.index:
+                corp_df.at[i,'d'] = coriolis_parameter(lat_df.at[i,'d']).astype(float32)
+
+            df_list.append(corp_df)
+
+        return final_results(df_list, CoriolisParameterError, self.meta_df)
+
+def ajust_column_values(current_group, corp_df):
+    corp_df['typvar'] = current_group.typvar.unique()[0]
+    corp_df['grtyp'] = current_group.grtyp.unique()[0]
+    corp_df['ig1'] = current_group.ig1.unique()[0]
+    corp_df['ig2'] = current_group.ig2.unique()[0]
+    corp_df['ig3'] = current_group.ig3.unique()[0]
+    corp_df['ig4'] = current_group.ig4.unique()[0]
+    return corp_df
---
+++ test/coriolisparameter_test.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from test import TMP_PATH,TEST_PATH
+import pytest
+import fstpy.all as fstpy
+import spookipy.all as spooki
+import pandas as pd
+
+pytestmark = [pytest.mark.regressions]
+
+@pytest.fixture
+def plugin_test_dir():
+    return TEST_PATH + '/CoriolisParameter/testsFiles/'
+
+
+
+def test_1(plugin_test_dir):
+    """Test #1 : Calculate with a simple test data """
+    # open and read source
+    source0 = plugin_test_dir + "UUVVTT_fileSrc.std"
+    src_df0 = fstpy.StandardFileReader(source0).to_pandas()
+
+
+    #compute CoriolisParameter
+    df = spooki.CoriolisParameter(src_df0).compute()
+    #[ReaderStd --input {sources[0]}] >> [CoriolisParameter] >> [WriterStd --output {destination_path} ]
+    df.loc[:,'etiket'] = 'R1558V0_N'
+    df.loc[df.nomvar=='CORP','ip1'] = 32505856
+    df.loc[df.nomvar=='CORP','etiket'] = '__CORIOPX'
+
+    # df.loc[:,'datyp'] = 5
+    # df.loc[:,'nbits'] = 32
+
+    #write the result
+    results_file = TMP_PATH + "test_1.std"
+    fstpy.delete_file(results_file)
+    fstpy.StandardFileWriter(results_file, df).to_fst()
+
+    # open and read comparison file
+    file_to_compare = plugin_test_dir + "coriop_file2cmp_test_1.std"
+    # file_to_compare = '/home/sbf000/data/testFiles/CoriolisParameter/test_1'
+
+    #compare results
+    res = fstpy.fstcomp(results_file,file_to_compare,e_max=1.0000000001)
+    fstpy.delete_file(results_file)
+    assert(res == True)
+
+
+def test_2(plugin_test_dir):
+    """Test #2 : Spooki must succeed when inputs are in millibars"""
+    # open and read source
+    source0 = plugin_test_dir + "2011100712_012_regpres"
+    src_df0 = fstpy.StandardFileReader(source0).to_pandas()
+
+
+    #compute CoriolisParameter
+    df = spooki.CoriolisParameter(src_df0).compute()
+    #[ReaderStd --input {sources[0]}] >>[CoriolisParameter]
+
+    assert(not df.empty)

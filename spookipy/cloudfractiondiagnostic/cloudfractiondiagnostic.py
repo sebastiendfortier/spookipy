f1cc92963993dd351ee322c4af775f2bceb522bb
Sébastien Fortier <sebastien.fortier@canada.ca>
2021-08-12 14:36:59 +0000

    add cloudfraction diagnostic plugin

---
+++ spookipy/cloudfractiondiagnostic/cloudfractiondiagnostic.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+from ..utils import create_empty_result, get_existing_result, get_plugin_dependencies, existing_results, final_results, initializer
+from ..plugin import Plugin
+import pandas as pd
+import fstpy.all as fstpy
+import sys
+import numpy as np
+import math
+
+
+def calc_diagnostic_cloud_fraction_threshold(level):
+    return 1 - 2 * level + math.pow(level, 2) + math.pow(level, 3) + math.sqrt(3.0) * level * (1 - 3 * level + 2 * math.pow(level, 2))
+
+def calc_diagnostic_cloud_fraction(hr,threshold):
+    cld = np.full_like(hr, -1,dtype=np.float32)
+    cld = np.where(hr <= threshold, 0, cld)
+    cld = np.where((threshold < hr) & (hr < 1), ((hr - threshold) / (1 - threshold))**2, cld)
+    cld = np.where(hr >= 1, 1, cld)
+    return cld.astype(np.float32)
+
+class CloudFractionDiagnosticError(Exception):
+    pass
+
+class CloudFractionDiagnostic(Plugin):
+
+    @initializer
+    def __init__(self,df:pd.DataFrame,use_constant=None):
+        self.plugin_mandatory_dependencies = {
+            'HR':{'nomvar':'HR','unit':'scalar','select_only':True},
+        }
+
+        self.plugin_result_specifications = {
+            'CLD':{'nomvar':'CLD','etiket':'CloudFractionDiagnostic','unit':'scalar'}
+        }
+
+        self.validate_input()
+
+    def validate_input(self):
+        if self.df.empty:
+            raise CloudFractionDiagnosticError('No data to process')
+
+        self.df = fstpy.metadata_cleanup(self.df)
+
+        self.df = fstpy.add_composite_columns(self.df,True,'numpy', attributes_to_decode=['unit','forecast_hour','ip_info'])
+
+        # print(self.df[['nomvar','typvar','etiket','unit','surface','grid','forecast_hour']].sort_values(by=['grid','nomvar']).to_string())
+        self.meta_df = self.df.loc[self.df.nomvar.isin(["^^",">>","^>", "!!", "!!SF", "HY","P0","PT"])].reset_index(drop=True)
+
+        self.df = self.df.loc[~self.df.nomvar.isin(["^^",">>","^>", "!!", "!!SF", "HY","P0","PT"])].reset_index(drop=True)
+
+        #check if result already exists
+        self.existing_result_df = get_existing_result(self.df,self.plugin_result_specifications)
+
+        if self.existing_result_df.empty:
+            self.dependencies_df = get_plugin_dependencies(self.df,None,self.plugin_mandatory_dependencies)
+
+
+    def compute(self) -> pd.DataFrame:
+        if not self.existing_result_df.empty:
+            return existing_results('CloudFractionDiagnostic',self.existing_result_df,self.meta_df)
+
+        sys.stdout.write('CloudFractionDiagnostic - compute\n')
+
+        self.dependencies_df = fstpy.load_data(self.dependencies_df)
+
+        cld_df = create_empty_result(self.dependencies_df,self.plugin_result_specifications['CLD'],copy=True)
+
+        df_list=[]
+
+        if not (self.use_constant is None):
+            for  i in cld_df.index:
+                cld_df.at[i,'d'] = np.full_like(cld_df.at[i,'d'],0.8,dtype=np.float32)
+        else:
+            for  i in cld_df.index:
+                level = cld_df.at[i,'level']
+                hr = np.copy(cld_df.at[i,'d'])
+                threshold = calc_diagnostic_cloud_fraction_threshold(level)
+                cld_df.at[i,'d'] = calc_diagnostic_cloud_fraction(hr,threshold)
+
+        df_list.append(cld_df)
+
+        return final_results(df_list, CloudFractionDiagnosticError, self.meta_df)
---
+++ test/cloudfractiondiagnostic_test.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import fstpy.all as fstpy
+import pytest
+from test import TMP_PATH,TEST_PATH
+
+import spookipy.all as spooki
+
+pytestmark = [pytest.mark.regressions]
+
+@pytest.fixture
+def plugin_test_dir():
+    return TEST_PATH + '/CloudFractionDiagnostic/testsFiles/'
+
+
+
+def test_1(plugin_test_dir):
+    """Test #1 : Test regulier des donnees venant avec la demande de plugin"""
+    # open and read source
+    source0 = plugin_test_dir + "HR_27_fileSrc.std"
+    src_df0 = fstpy.StandardFileReader(source0).to_pandas()
+
+
+    #compute CloudFractionDiagnostic
+    df = spooki.CloudFractionDiagnostic(src_df0).compute()
+    #[ReaderStd --input {sources[0]}] >> [CloudFractionDiagnostic] >>
+    # [Zap --pdsLabel CLDFRACTION --doNotFlagAsZapped] >>
+    # [WriterStd --output {destination_path} --ignoreExtended --IP1EncodingStyle OLDSTYLE]
+
+    df.loc[:,'etiket'] = 'CLDFRACTION'
+    # df.loc[:,'datyp'] = 5
+    # df.loc[:,'nbits'] = 32
+
+    #write the result
+    results_file = TMP_PATH + "test_cfd_1.std"
+    fstpy.delete_file(results_file)
+    fstpy.StandardFileWriter(results_file, df).to_fst()
+
+    # open and read comparison file
+    file_to_compare = plugin_test_dir + "cfd_file2cmp.std"
+    # file_to_compare = "/home/sbf000/data/testFiles/CloudFractionDiagnostic/result_test_cfd_1"
+
+    #compare results
+    res = fstpy.fstcomp(results_file,file_to_compare)
+    fstpy.delete_file(results_file)
+    assert(res == True)

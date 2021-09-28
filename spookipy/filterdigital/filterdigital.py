b13a348a205ba20f615cb6ce5a53a8c6a37d7c17
Sébastien Fortier <sebastien.fortier@canada.ca>
2021-09-28 12:54:37 +0000

    modified to work with dask arrays

--- spookipy/filterdigital/filterdigital.py
+++ spookipy/filterdigital/filterdigital.py
@@ -4,22 +4,28 @@ import logging
 import numpy as np
 import pandas as pd
 import dask.array as da
-
+import fstpy.all as fstpy
 from ..plugin import Plugin
-from ..utils import (final_results, initializer, validate_nomvar)
+from ..utils import (final_results, initializer, to_dask, to_numpy, validate_nomvar)
 from .f_stenfilt import f_stenfilt
 
 
 class FilterDigitalError(Exception):
     pass
 
+
 class FilterDigital(Plugin):
 
     @initializer
-    def __init__(self,df:pd.DataFrame,filter:list,repetitions:int=1,nomvar_out=None):
+    def __init__(
+            self,
+            df: pd.DataFrame,
+            filter: list,
+            repetitions: int = 1,
+            nomvar_out=None):
 
         self.plugin_result_specifications = {
-            'ALL':{'filtered':True}
+            'ALL': {'filtered': True}
             # 'etiket':'FLTRDG',
         }
 
@@ -29,20 +35,22 @@ class FilterDigital(Plugin):
         if self.df.empty:
             raise FilterDigitalError('No data to process')
 
-        validate_nomvar(self.nomvar_out,'FilterDigital',FilterDigitalError)
+        validate_nomvar(self.nomvar_out, 'FilterDigital', FilterDigitalError)
 
         if not len(self.filter):
             raise FilterDigitalError('Filter must contain at least 1 value')
 
-        if len(self.filter)%2==0:
+        if len(self.filter) % 2 == 0:
             raise FilterDigitalError('Filter lenght must be odd, not even')
 
         if not (self.repetitions > 0):
             raise FilterDigitalError('Repetitions must be a positive integer')
 
-        self.meta_df = self.df.loc[self.df.nomvar.isin(["^^",">>","^>", "!!", "!!SF", "HY","P0","PT"])].reset_index(drop=True)
+        self.meta_df = self.df.loc[self.df.nomvar.isin(
+            ["^^", ">>", "^>", "!!", "!!SF", "HY", "P0", "PT"])].reset_index(drop=True)
 
-        self.df = self.df.loc[~self.df.nomvar.isin(["^^",">>","^>", "!!", "!!SF", "HY","P0","PT"])].reset_index(drop=True)
+        self.df = self.df.loc[~self.df.nomvar.isin(
+            ["^^", ">>", "^>", "!!", "!!SF", "HY", "P0", "PT"])].reset_index(drop=True)
 
     def compute(self) -> pd.DataFrame:
         logging.info('FilterDigital - compute')
@@ -51,28 +59,30 @@ class FilterDigital(Plugin):
         #     self.plugin_result_specifications['ALL']['nomvar'] = self.nomvar_out
 
         # self.df = fstpy.compute(self.df)
-        
+
         if not (self.nomvar_out is None):
-            self.df['nomvar'] = self.nomvar_out    
+            self.df['nomvar'] = self.nomvar_out
         self.df['filtered'] = True
-        
 
         # new_df = create_empty_result(self.df,self.plugin_result_specifications['ALL'],all_rows=True)
 
-        
-
         filter_len = len(self.filter)
 
-        filter = np.array(self.filter,dtype=np.int32,order='F')
+        filter = np.array(self.filter, dtype=np.int32, order='F')
 
-        df_list=[]
+        df_list = []
         for i in self.df.index:
-            ni = self.df.at[i,'d'].shape[0]
-            nj = self.df.at[i,'d'].shape[1]
-            if not isinstance(self.df.at[i,'d'],np.ndarray):
-                self.df.at[i,'d'] = self.df.at[i,'d'].compute()
-            f_stenfilt(slab=self.df.at[i,'d'],ni=ni,nj=nj,npass=self.repetitions,list=filter,l=filter_len)
-            self.df.at[i,'d'] = da.from_array(self.df.at[i,'d'].astype(np.float32))
+            ni = self.df.at[i, 'd'].shape[0]
+            nj = self.df.at[i, 'd'].shape[1]
+            self.df.at[i, 'd'] = to_numpy(self.df.at[i, 'd'])
+            f_stenfilt(slab=self.df.at[i,
+                                       'd'],
+                       ni=ni,
+                       nj=nj,
+                       npass=self.repetitions,
+                       list=filter,
+                       l=filter_len)
+            self.df.at[i, 'd'] = to_dask(self.df.at[i, 'd'])
 
         df_list.append(self.df)
 

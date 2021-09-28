791b69bb4f066ecbcbafb8f122406b1bd339423d
Sébastien Fortier <sebastien.fortier@canada.ca>
2021-09-28 12:35:49 +0000

    add package version

--- spookipy/__init__.py
+++ spookipy/__init__.py
@@ -1,1 +1,9 @@
 # -*- coding: utf-8 -*-
+import os
+from pathlib import Path
+
+
+p = Path(os.path.abspath(__file__))
+v_file = open(p.parent.parent / 'VERSION')
+__version__ = v_file.readline().strip()
+v_file.close()

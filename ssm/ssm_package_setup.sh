e93ab47c39c3dce3b3db1d11def88ae028046669
Sébastien Fortier <sebastien.fortier@canada.ca>
2021-10-19 13:27:37 +0000

    no dep auto load for ssm package

--- ssm/ssm_package_setup.sh
+++ ssm/ssm_package_setup.sh
@@ -1,5 +1,5 @@
 main(){
-      load_spookipy_runtime_dependencies
+      load_runtime_dependencies
 }
 
 message(){
@@ -12,10 +12,14 @@ print_and_do(){
    eval $@
 }
 
-load_spookipy_runtime_dependencies(){
-    message "Loading fstpy runtime dependencies ..."
-    print_and_do . r.load.dot eccc/mrd/rpn/MIG/ENV/migdep/5.1.1
-    print_and_do . r.load.dot eccc/mrd/rpn/MIG/ENV/rpnpy/2.1.2
+load_runtime_dependencies(){
+    message "Load rpnpy, fstpy and ci_fstcomp if you dont have them ..."
+    message . r.load.dot eccc/mrd/rpn/MIG/ENV/migdep/5.1.1
+    message . r.load.dot eccc/mrd/rpn/MIG/ENV/rpnpy/2.1.2
+    message . ssmuse-sh -d /fs/ssm/eccc/cmd/cmds/fstpy/(check directory for latest version)/
+    message . ssmuse-sh -d /fs/ssm/eccc/cmd/cmds/apps/ci_fstcomp/(check directory for latest version)/
+   #  print_and_do . r.load.dot eccc/mrd/rpn/MIG/ENV/migdep/5.1.1
+   #  print_and_do . r.load.dot eccc/mrd/rpn/MIG/ENV/rpnpy/2.1.2
 }
 
 main

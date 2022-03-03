043a1f5d21df00d156778a091d113f23ffc43153
Logan Yu <logan.yu@ec.gc.ca>
2022-03-03 13:48:30 +0000

    Update test function to reflect on file name change

--- test/percentiletopercentage_test.py
+++ test/percentiletopercentage_test.py
@@ -17,19 +17,19 @@ def plugin_test_dir():
 def test_1(plugin_test_dir):
     """Test with default options """
     # open and read source
-    source0 = plugin_test_dir + "2022021100_out"
+    source0 = plugin_test_dir + "2022021100_out.std"
     src_df0 = fstpy.StandardFileReader(source0).to_pandas()
 
     df = spooki.PercentileToPercentage(src_df0).compute()
     # [ReaderStd --input {sources[0]}] >> [PercentileToPercentage] >> [WriterStd --output {destination_path} ]
 
     # write the result
-    results_file = TMP_PATH + "test_1"
+    results_file = TMP_PATH + "test_1.std"
     fstpy.delete_file(results_file)
     fstpy.StandardFileWriter(results_file, df).to_fst()
 
     # open and read comparison file
-    file_to_compare = plugin_test_dir + "PercentileToPercentage_file1cmp"
+    file_to_compare = plugin_test_dir + "PercentileToPercentage_file1cmp.std"
 
     # compare results
     res = fstcomp(results_file, file_to_compare, e_max=0.001)
@@ -40,7 +40,7 @@ def test_1(plugin_test_dir):
 def test_2(plugin_test_dir):
     """Test with an incorrect eteiket name"""
     # open and read source
-    source0 = plugin_test_dir + "2022021100_out"
+    source0 = plugin_test_dir + "2022021100_out.std"
     src_df0 = fstpy.StandardFileReader(source0).to_pandas()
 
     with pytest.raises(Exception):
@@ -51,7 +51,7 @@ def test_2(plugin_test_dir):
 def test_3(plugin_test_dir):
     """Test with changes to nomvar and operator"""
     # open and read source
-    source0 = plugin_test_dir + "2022021100_out"
+    source0 = plugin_test_dir + "2022021100_out.std"
     src_df0 = fstpy.StandardFileReader(source0).to_pandas()
 
     df = spooki.PercentileToPercentage(src_df0, threshold=0.3, operator='le', etiket='GE0_____PALL',
@@ -59,12 +59,12 @@ def test_3(plugin_test_dir):
     # [ReaderStd --input {sources[0]}] >> [Threshold --threshold 0.3, Operator --operator le, Etiket --etiket GE0_____PALL, Nomvar --nomvar SSH8, Typvar --typvar P@, Percentile_Step --percentile_step 0,100,5] >> [PercentileToPercentage] >> [WriterStd --output {destination_path} ]
 
     # write the result
-    results_file = TMP_PATH + "test_2"
+    results_file = TMP_PATH + "test_2.std"
     fstpy.delete_file(results_file)
     fstpy.StandardFileWriter(results_file, df).to_fst()
 
     # open and read comparison file
-    file_to_compare = plugin_test_dir + "PercentileToPercentage_file3cmp"
+    file_to_compare = plugin_test_dir + "PercentileToPercentage_file3cmp.std"
 
     # compare results
 
@@ -76,7 +76,7 @@ def test_3(plugin_test_dir):
 def test_4(plugin_test_dir):
     """Test with non valid percentile_step"""
     # open and read source
-    source0 = plugin_test_dir + "2022021100_out"
+    source0 = plugin_test_dir + "2022021100_out.std"
     src_df0 = fstpy.StandardFileReader(source0).to_pandas()
 
     with pytest.raises(Exception):
@@ -88,19 +88,19 @@ def test_5(plugin_test_dir):
     """Test with negative threshold value and SSH8 nomvar"""
     
     # open and read source
-    source0 = plugin_test_dir + "2022021100_out"
+    source0 = plugin_test_dir + "2022021100_out.std"
     src_df0 = fstpy.StandardFileReader(source0).to_pandas()
 
     df = spooki.PercentileToPercentage(src_df0, threshold=-0.3, nomvar='SSH8').compute()
     # [ReaderStd --input {sources[0]}] >> [Threshold --threshold -0.3, Nomvar --nomvar SSH8] >> [PercentileToPercentage] >> [WriterStd --output {destination_path} ]
 
     # write the result
-    results_file = TMP_PATH + "test_5"
+    results_file = TMP_PATH + "test_5.std"
     fstpy.delete_file(results_file)
     fstpy.StandardFileWriter(results_file, df).to_fst()
 
     # open and read comparison file
-    file_to_compare = plugin_test_dir + "PercentileToPercentage_file5cmp"
+    file_to_compare = plugin_test_dir + "PercentileToPercentage_file5cmp.std"
 
     # compare results
     res = fstcomp(results_file, file_to_compare, e_max=0.001)

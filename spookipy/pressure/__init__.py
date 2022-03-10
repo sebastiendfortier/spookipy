559512b487f7a54c316b952af7d3e60bcfac9978
Sébastien Fortier <sebastien.fortier@canada.ca>
2022-03-10 16:31:49 +0000

    fix imports

--- spookipy/pressure/__init__.py
+++ spookipy/pressure/__init__.py
@@ -1,1 +1,1 @@
-from .pressure import Pressure, compute_pressure_from_eta_coord_array, compute_pressure_from_hyb_coord_array, compute_pressure_from_hybstag_coord_array, compute_pressure_from_pressure_coord_array, compute_pressure_from_sigma_coord_array
+from .pressure import Pressure

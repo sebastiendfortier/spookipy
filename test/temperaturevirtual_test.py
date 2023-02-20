1cd745d507fc1d853342f8fcf41db9e08a095447
Audrey Germain <audrey.germain@ec.gc.ca>
2023-02-20 23:15:55 +0000

    remove debug code

--- test/temperaturevirtual_test.py
+++ test/temperaturevirtual_test.py
@@ -74,17 +74,9 @@ def test_2(plugin_test_dir):
     # open and read comparison file
     file_to_compare = plugin_test_dir + "resulttest_2.std"
 
-    # pd.set_option("display.max_rows", 500, "display.max_columns", 500)
-
-    # src_df1 = fstpy.StandardFileReader(results_file).to_pandas()
-    # src_df2 = fstpy.StandardFileReader(file_to_compare).to_pandas()
-    # print(src_df1)
-    # print(src_df2)
-
     # compare results
     res = fstcomp(results_file, file_to_compare, e_max=0.01)
-    # fstpy.delete_file(results_file)
-    print(results_file)
+    fstpy.delete_file(results_file)
     assert(res)
 
 # Les tests suivants sont nouveaux (n'existent pas dans Spooki)

04282c6bc4b7880394f09064ebb55da038e3ebe3
Guylaine Hardy <guylaine.hardy@canada.ca>
2023-07-21 14:07:20 +0000

    Ajout du level pour former les groupements.

--- spookipy/timeintervalminmax/timeintervalminmax.py
+++ spookipy/timeintervalminmax/timeintervalminmax.py
@@ -123,8 +123,8 @@ class TimeIntervalMinMax(Plugin):
         self.df_with_intervals = self.df.loc[(~self.df.nomvar.isin(
             ["^^", ">>", "^>", "!!", "!!SF", "HY", "P0", "PT"])) & (~self.df.interval.isna()) & (self.df.nomvar.isin(self.nomvar))].reset_index(drop=True)
 
-        self.groups_without_interval = self.df_without_intervals.groupby(['grid', 'nomvar','ip1_kind'])
-        self.groups_with_interval    = self.df_with_intervals.groupby(['grid', 'nomvar','ip1_kind'])
+        self.groups_without_interval = self.df_without_intervals.groupby(['grid', 'nomvar','level','ip1_kind'])
+        self.groups_with_interval    = self.df_with_intervals.groupby(['grid', 'nomvar','level', 'ip1_kind'])
 
 
     def compute(self) -> pd.DataFrame:
@@ -191,7 +191,7 @@ class TimeIntervalMinMax(Plugin):
         return final_results(df_list, TimeIntervalMinMaxError, self.meta_df)
 
     def process(self, current_group, interval_df, b_inf, b_sup):
-        arr3d = da.stack(interval_df['d'])
+        arr3d = da.stack(interval_df['d']) 
         results = []
         if self.min:
             # set new ip2, ip3 and npas
@@ -270,7 +270,7 @@ def create_result_container(df, b_inf, b_sup, nomvar):
 def check_for_negative_values(arr, location):
     if np.any(np.where(arr < 0.,True,False)):
         logging.warning(f"Found a negative value in the {location}! Probable cause is loss of precision when converting to float computational type")
-        logging.warning(f'The lowest found negative value was : {np.min(arr)}')
+        logging.warning(f'The lowest found negative value was : {to_numpy(np.min(arr))}')
 
 
 def get_lower_bound(interval):

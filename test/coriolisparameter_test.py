a5a9846ce92b3a40cd1836cb13e9eac5296b89c9
Audrey Germain <audrey.germain@ec.gc.ca>
2023-10-20 22:04:50 +0000

    add coriolis paramenter tests with data as a ndarray

--- test/coriolisparameter_test.py
+++ test/coriolisparameter_test.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import numpy as np
 from test import TEST_PATH, TMP_PATH, check_test_ssm_package
 
 check_test_ssm_package()
@@ -57,6 +58,67 @@ def test_2(plugin_test_dir):
     df.loc[df.nomvar == 'CORP', 'etiket'] = '__CORIOPX'
     
     results_file = ''.join([TMP_PATH, secrets.token_hex(16), "test_2.std"])
+    fstpy.delete_file(results_file)
+    fstpy.StandardFileWriter(results_file, df).to_fst()
+
+    # open and read comparison file
+    file_to_compare = plugin_test_dir + "coriop_file2cmp_test_2_20230124.std"
+
+    # compare results
+    res = fstcomp(results_file, file_to_compare) 
+    fstpy.delete_file(results_file)
+    assert(res)
+
+def test_3(plugin_test_dir):
+    """Calculate with a simple test data with d as a ndarray"""
+    # open and read source
+    source0 = plugin_test_dir + "UUVVTT_fileSrc.std"
+    src_df0 = fstpy.StandardFileReader(source0).to_pandas()
+
+    src_df0 = fstpy.compute(src_df0)
+    src_df0.loc[:, 'path'] = None
+    src_df0.loc[:, 'key'] = None
+    src_df0['d'] = src_df0.apply(lambda row: np.asarray(row['d']),axis=1)
+
+    # compute CoriolisParameter
+    df = spookipy.CoriolisParameter(src_df0).compute()
+    # [ReaderStd --input {sources[0]}] >> [CoriolisParameter] >> [WriterStd --output {destination_path} ]
+    df.loc[:, 'etiket'] = 'R1558V0_N'
+    df.loc[df.nomvar == 'CORP', 'etiket'] = '__CORIOPX'
+
+    # write the result
+    results_file = ''.join([TMP_PATH, secrets.token_hex(16), "test_3.std"])
+    fstpy.delete_file(results_file)
+    fstpy.StandardFileWriter(results_file, df).to_fst()
+
+    # open and read comparison file
+    file_to_compare = plugin_test_dir + "coriop_file2cmp_test_1_20230124.std"
+
+    # compare results
+    res = fstcomp(results_file, file_to_compare)  # ,e_max=0.13)
+    fstpy.delete_file(results_file)
+    assert(res)
+
+
+def test_4(plugin_test_dir):
+    """Spooki must succeed when inputs are in millibars with d as a ndarray"""
+    # open and read source
+    source0 = plugin_test_dir + "2011100712_012_regpres"
+    src_df0 = fstpy.StandardFileReader(source0).to_pandas()
+    src_df0 = fstpy.select_with_meta(src_df0, ["UU"])
+
+    src_df0 = fstpy.compute(src_df0)
+    src_df0.loc[:, 'path'] = None
+    src_df0.loc[:, 'key'] = None
+    src_df0['d'] = src_df0.apply(lambda row: np.asarray(row['d']),axis=1)
+
+    # compute CoriolisParameter
+    df = spookipy.CoriolisParameter(src_df0).compute()
+    
+    df.loc[:, 'etiket'] = 'R1580V0_N'
+    df.loc[df.nomvar == 'CORP', 'etiket'] = '__CORIOPX'
+
+    results_file = ''.join([TMP_PATH, secrets.token_hex(16), "test_4.std"])
     fstpy.delete_file(results_file)
     fstpy.StandardFileWriter(results_file, df).to_fst()
 
